9afbbb9076e628602c3c625183fd9e2cd7396b20
Từ Thanh Tùng <tuthanhtungit@gmail.com>
2018-03-22 14:02:19 +0700

    update

--- api/api.py
+++ api/api.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify
 api = Blueprint('api', 'aaa', url_prefix='/api')
 
 
-@api.route('/test')
+@api.route('/index')
 def index():
     return jsonify({"status": 200, "message": "this is message"})
 
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask
 
 from api.api import api
 
@@ -7,28 +7,6 @@ app = Flask(__name__)
 app.register_blueprint(api)
 
 flask_app = Flask(__name__)
-
-
-@app.route('/user/<username>', methods=['GET', 'POST'])
-def show(username):
-    return username
-
-
-def do_the_login():
-    return "123"
-
-
-def show_the_login_form():
-    return render_template('login.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
