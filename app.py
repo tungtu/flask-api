bb567e825b5b4b2064e1d60c927ce083d405f4c8
Từ Thanh Tùng <tuthanhtungit@gmail.com>
2018-03-22 14:36:37 +0700

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 from api.api import api
@@ -8,5 +9,7 @@ app.register_blueprint(api)
 
 flask_app = Flask(__name__)
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
